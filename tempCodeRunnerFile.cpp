cb3c5d2475b3b037f18eac2a25ba3eac2f9365b6
Guilherme Nicolasi <guilherme_nicolasi@hotmail.com.br>
2023-09-20 16:11:15 -0300

    sequential-v11

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-       for (j = 0; j < MAX_SIZE; j++) {
-            if (newGeneration->grid[i][j] != generation->grid[i][j]) {
-                return false;  // As gerações são diferentes
-            }
-        }
-    }
-
-    return true;  // As gerações são iguais
-}
